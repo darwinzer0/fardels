3bb1e72605f2040ebb18124e03f7885442cbaf1e
Ben Adams <darwinzero@gmail.com>
2021-04-24 10:56:47 +1200

    move client to different repository

--- client/src/Const.js
+++
@@ -1,7 +0,0 @@
-export const DRAWER_MENU_VIEW_HANDLE = 0;
-export const DRAWER_MENU_FOLLOWING = 1;
-export const DRAWER_MENU_MY_FARDELS = 2;
-export const DRAWER_MENU_PROFILE = 3;
-export const DRAWER_MENU_ABOUT = 4;
-export const DRAWER_MENU_UNPACKED_FARDELS = 5;
-export const DRAWER_MENU_NEW_FARDEL = 6;
